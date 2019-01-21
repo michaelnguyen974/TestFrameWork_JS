0ad95c68bc96b852d992859a2efd744f052e04e5
Michael Nguyen <michaelnguyen974@gmail.com>
2019-01-21 17:06:03 +0000

    Expect matcher now added

--- spec/SquareSpec.js
+++ spec/SquareSpec.js
@@ -1,7 +1,6 @@
 describe("Area", () => {
   it("can find area", () => {
     square = new Square(5);
-    toBeIdenticalTo(square.area(), 25)
-
+    expect(square.area()).toBeIdenticalTo(25)
   });
 });
