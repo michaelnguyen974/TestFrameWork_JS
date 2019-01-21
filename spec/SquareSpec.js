730bd377cd1539f50fa19339dede341044519d97
Michael Nguyen <michaelnguyen974@gmail.com>
2019-01-21 17:10:09 +0000

    changed fail message of toBeIdenticalTo property

--- spec/SquareSpec.js
+++ spec/SquareSpec.js
@@ -1,6 +1,6 @@
 describe("Area", () => {
   it("can find area", () => {
     square = new Square(5);
-    expect(square.area()).toBeIdenticalTo(25)
+    expect(square.area()).toBeIdenticalTo(23)
   });
 });
